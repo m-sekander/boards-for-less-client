d2fe9b2b6e0a96397ee7a056fece203e3edfc5e9
Moin Sekander <moinuddin8510@gmail.com>
2022-10-08 20:04:12 -0500

    absolve react warning messages

--- src/components/Rent/Rent.js
+++ src/components/Rent/Rent.js
@@ -48,7 +48,7 @@ function Rent() {
             }).catch((error) => {
                 console.log("For devs:", error);
             })
-    }, [])
+    }, [token, serverPort])
 
     useEffect(() => {
         axios.post(`https://www.googleapis.com/geolocation/v1/geolocate?key=${process.env.REACT_APP_GOOGLE_KEY}`, {})
@@ -85,7 +85,7 @@ function Rent() {
             setCoordinates(null);
         })
         })
-    }, [token, emptySearch, serverPort])
+    }, [token, emptySearch, serverPort, homeLocation]) // eslint-disable-line react-hooks/exhaustive-deps
 
     function handleSearch(event) {
         event.preventDefault();
